71be3f4b9d5377039504f67fabd33d187e4be311
legendvi <dvigneshkumar3@gmail.com>
2021-06-25 16:46:59 +0530

    initial commit

---
+++ script.js
@@ -0,0 +1,101 @@
+/*console.log('Hi')
+
+console.log(age1(1990))
+
+function age1(birth) {
+    return birth - 2000
+}
+
+
+age2 = function (birth) {
+    return birth - 1990
+}
+dif = age2(1900)
+
+console.log(dif)
+
+age3 = function (birth, dif) {
+    a = birth - 2037
+    b = a - dif
+    return 1000
+}
+
+dif1 = age3(1900, 200)
+console.log(dif1)
+*/
+
+
+const sec_num = Math.trunc(Math.random() * 20) + 1
+console.log(sec_num)
+document.querySelector('.sec-num').textContent = '?'
+score = 5
+hscore = 0
+T = 1
+document.querySelector('.score').textContent = score
+gus = document.querySelector('.start').textContent
+
+
+
+
+btn = document.querySelector('.btn')
+
+btn.addEventListener('click', function () {
+    s = Number(document.querySelector('.number').value)
+    console.log(s, typeof (s))
+
+
+    if (s == sec_num || T == 0) {
+        document.querySelector('.start').textContent = 'Hurray!! you won the Game :).click play again to play again'
+        document.querySelector('body').style.backgroundImage = 'linear-gradient(green, rgb(115, 167, 115))';
+        document.querySelector('.sec-num').textContent = sec_num
+        T = 0
+        if (score > hscore) {
+            hscore = score
+            document.querySelector('.hscore').textContent = hscore
+        }
+    }
+
+    else if (String(s) == '') {
+        gus = 'No Number Given'
+        document.querySelector('.start').textContent = 'No Number Entered'
+    }
+    else if (score == 0) {
+        document.querySelector('.start').textContent = 'you lost the Game. Try Again🙂'
+        document.querySelector('body').style.backgroundImage = 'linear-gradient(crimson,red)';
+        document.querySelector('.sec-num').textContent = sec_num
+
+    }
+
+    else if (s < 1 || s > 20) {
+        document.querySelector('.start').textContent = 'Number not in right Range🙂'
+        score -= 1
+    }
+
+    else if (s > sec_num) {
+        document.querySelector('.start').textContent = 'Too High'
+        score -= 1
+        document.querySelector('.score').textContent = score
+    }
+    else if (s < sec_num) {
+        document.querySelector('.start').textContent = "Too Low"
+        score -= 1
+        document.querySelector('.score').textContent = score
+    }
+
+}
+
+)
+
+document.querySelector('.play').addEventListener('click', function () {
+    const sec_num = Math.trunc(Math.random() * 20) + 1
+    console.log(sec_num)
+    document.querySelector('.sec-num').textContent = '?'
+    score = 5
+    T = 1
+    document.querySelector('.score').textContent = score
+    gus = document.querySelector('.start').textContent
+
+    document.querySelector('.start').textContent = 'Start Guessing...'
+    s = document.querySelector('.number').value = ''
+    document.querySelector('body').style.backgroundImage = 'linear-gradient(rgb(22, 157, 219), rgb(25, 34, 165))';
+})
